736bea97d50f8c91459c8ae518eca6e06e629c5b
tnhubbar <taryn.johnson919@gmail.com>
2020-05-02 14:22:35 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :articles, only: [:index, :show, :new, :create]
+  get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
+  patch 'articles/:id', to: 'articles#update'
+
+  #You could add resources :articles, only: [:index, :show, :new, :create, :edit, :update] and it would do the same thing.
 end
